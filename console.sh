2268b8b14b9f2df1194e9b2a2c9e8b49ccaef6d4
m.dinh10k <mdinh10k@gmail.com>
2018-09-25 02:09:40 +0700

    a

--- console.sh
+++ console.sh
@@ -5,5 +5,5 @@ clear
 
 export PYTHONPATH=./src:../program-y/src:../template-y/libs/MetOffer-1.3.2:.
 
-python3 ../program-y/src/programy/clients/console.py --config ./config.yaml --cformat yaml --logging ./logging.yaml
+python3 ./src/conditionalfileloader/new_chat_bot.py --config ./config.yaml --cformat yaml --logging ./logging.yaml --bot_root .
 
---
+++ src/conditionalfileloader/new_chat_bot.py
@@ -0,0 +1,75 @@
+from programy.utils.logging.ylogger import YLogger
+
+from programy.clients.events.client import EventBotClient
+from programy.clients.events.console.config import ConsoleConfiguration
+
+class NewChatBot(EventBotClient):
+    def __init__(self, arg_parser=None):
+        self.running = False
+        EventBotClient.__init__(self, "console", arg_parser)
+
+
+    def get_description(self):
+        return 'ProgramY AIML2.0 Console Client'
+
+    def get_client_configuration(self):
+        return ConsoleConfiguration()
+
+    def add_client_arguments(self, parser=None):
+        return
+
+    def parse_args(self, arguments, parsed_args):
+        return
+
+    def get_question(self, client_context, input_func=input):
+        ask = "%s " % self.get_client_configuration().prompt
+        return input_func(ask)
+
+    def display_startup_messages(self, client_context):
+        self.process_response(client_context, client_context.bot.get_version_string(client_context))
+        initial_question = client_context.bot.get_initial_question(client_context)
+        self._renderer.render(client_context, initial_question)
+
+    def process_question(self, client_context, question):
+        # Returns a response
+        return client_context.bot.ask_question(client_context , question, responselogger=self)
+
+    def render_response(self, client_context, response):
+        # Calls the renderer which handles RCS context, and then calls back to the client to show response
+        self._renderer.render(client_context, response)
+
+    def process_response(self, client_context, response):
+        print(response)
+
+    def process_question_answer(self, client_context):
+        question = self.get_question(client_context)
+        response = self.process_question(client_context, question)
+        self.render_response(client_context, response)
+
+    def wait_and_answer(self):
+        running = True
+        try:
+            client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
+            self.process_question_answer(client_context)
+        except KeyboardInterrupt as keye:
+            running = False
+            client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
+            self._renderer.render(client_context, client_context.bot.get_exit_response(client_context))
+        except Exception as e:
+            YLogger.exception(self, "Oops something bad happened !", e)
+        return running
+
+    def prior_to_run_loop(self):
+        client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
+        self.display_startup_messages(client_context)
+
+
+if __name__ == '__main__':
+
+    def run():
+        print("Loading, please wait...")
+        console_app = NewChatBot()
+        console_app.run()
+
+    run()
+    
